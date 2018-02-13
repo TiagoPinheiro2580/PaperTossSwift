b0f4f0eee9eb0eddc4a7e6f356babecb9ca2955d
Tiago Pinheiro <tiagopinheiro2580@hotmail.com>
2018-02-13 19:02:17 +0000

    Initial Commit

---
+++ PaperTossSlickTests/PaperTossSlickTests.swift
@@ -0,0 +1,36 @@
+//
+//  PaperTossSlickTests.swift
+//  PaperTossSlickTests
+//
+//  Created by Tiago Pinheiro on 13/02/18.
+//  Copyright © 2018 Tiago Pinheiro. All rights reserved.
+//
+
+import XCTest
+@testable import PaperTossSlick
+
+class PaperTossSlickTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
